e74031b9d6fea6b72d7ecfe03b4c3070f2744097
Rafli Ardiansyah <rrapliard@gmail.com>
2023-01-20 22:12:08 +0700

    fix posts

--- scraping.py
+++ scraping.py
@@ -13,18 +13,18 @@ def find_jobs():
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
-    for index, job in  enumerate(jobs):
+    for index, job in enumerate(jobs):
         published_date = job.find('span', class_ ='sim-posted').span.text
         company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
         skills = job.find('span', class_ ='srp-skills').text.replace(' ','')
         info = job.header.h2.a['href'] 
         if unfamiliar_skills not in skills:
-            with open('posts/{index}.txt','w') as f:
+            with open(f'posts/{index}.txt', 'w') as f:
                 f.write(f"Company: {company_name.strip()}")
                 f.write(f"Required Skills: {skills.strip()}")
-                f.write(f"More info: {info}")
-                f.write('')
-            print(f'file saved: {index}')
+                f.write(f'More info: {info}')
+                print('')
+            print(f'File saved: {index}')
 
 if __name__ == '__main__':
     while True:
